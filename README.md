1fe5e8400673fd3a7a384a85a164897230bdbf1a
binup <mankarabinu@gmail.com>
2022-03-14 13:25:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ixygfx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ixygfx)
---
+++ index.js
@@ -0,0 +1,12 @@
+// run `node index.js` in the terminal
+
+//indexA contains n number of integers
+let indexA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//indexB contains m number of integers
+let indexB = [2, 4, 6, 20];
+
+//find index common to both A and B and print to console
+let commonIndexes = [];
+
+console.log(commonIndexes); //[2, 4, 6]
